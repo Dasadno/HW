1741df922ac171b84be1a10b6b47e841b177c1cd
Dasadno <Yakaleks96@gmail.com>
2024-09-19 14:04:49 +0500

    ss

--- HW/main.cpp
+++ HW/main.cpp
@@ -28,6 +28,24 @@ int main() {
 	
 	*/
 
+	int num1;
+	int reversed_num;
+
+	std::cout << "Введите четырехзначное число: ";
+	std::cin >> num1;
+	if (num1 < 1000 || num1 > 9999) {
+		std::cout << "Попробуйте ввести четырехзначное число еще раз.";
+		return 0;
+	}
+	
+	std::cout << num1 / 1000 + num1 / 100 + num1 / 10 + num1 << "\n";
+	std::cout << num1 / 1000 % 10 << "\n";
+	std::cout << num1 / 100 % 10 << "\n";
+	std::cout << num1 / 10 % 10 << "\n";
+	std::cout << num1 % 10 << "\n";
+	
+	std::cout << reversed_num == ((num1 / 100 % 10 * 1000) + (num1 / 1000 % 10 * 100) + (num1 % 10 * 10) + (num1 / 10 % 10));
+
 
 	return 0;
 }
